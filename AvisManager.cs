38a153f2c8a02d3de38e267ad2b8a1f4a911b98e
aganne <56731603+aganne@users.noreply.github.com>
2020-06-04 06:09:53 +0200

    ajout du dossier Avis.Logic

---
+++ AvisManager.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Avis.Data;
+using Microsoft.SqlServer.Server;
+
+namespace Avis.Logic
+{
+    public class AvisManager
+    {
+        public void AjouterAvis(Avis.Data.Avis avis)
+        {
+            using (var context = new AvisEntities())
+            {
+                context.Avis.Add(avis);
+                context.SaveChanges();
+            }
+        }
+
+        public List<Avis.Data.Avis> ObtenirAvis(int trainingId)
+        {
+            List<Avis.Data.Avis> TousAvis = null;
+            using (var context = new AvisEntities())
+            {
+
+                if (context.Avis.Any(a => a.IdFormation == trainingId))
+                {
+                    TousAvis = new List<Avis.Data.Avis>();
+                    TousAvis = context.Avis.Where(a => a.IdFormation == trainingId).ToList();
+                }
+
+            }
+            return TousAvis;
+        }
+
+        public List<DetailAvis> ObtenirTousLesAvis()
+        {
+            List<DetailAvis> derniersAvisListe = null;
+            using (var context = new AvisEntities())
+            {
+
+                if (context.Avis.Any())
+                {
+                    derniersAvisListe = new List<DetailAvis>();
+                    var derniersAvis = context.Avis.OrderByDescending(a=>a.DateAvis).Take(10).ToList();
+                    if (derniersAvis!=null)
+                    {
+                        foreach (Avis.Data.Avis item in derniersAvis)
+                        {
+                            DetailAvis da = new DetailAvis(item.DateAvis);
+                            da.Formation = new Formation();
+                            da.Formation.Nom = item.Formation.Nom;
+                            da.Description = item.Description;
+                            da.Nom = item.Nom;
+                            derniersAvisListe.Add(da);
+                        }
+                    }
+                   
+                }
+
+            }
+            return derniersAvisListe;
+        }
+    }
+}
---
+++ FormationManager.cs
@@ -0,0 +1,92 @@
+﻿using Avis.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Avis.Logic
+{
+    public class FormationManager
+    {
+
+        public List<Formation> ObtenirFormations()
+        {
+            List<Formation> ToutesFormations = null;
+            using (var context = new AvisEntities())
+            {
+
+                if (context.Formation.Any())
+                {
+                    ToutesFormations = new List<Formation>();
+                    ToutesFormations = context.Formation.ToList();
+                }
+
+            }
+            return ToutesFormations;
+        }
+
+
+        /// <summary>
+        /// Affichage de 4 formations au hazard sur la page d'accueil
+        /// </summary>
+        public List<DetailFormation> ObtenirDetailFormations()
+        {
+            var detailFormationListe = new List<DetailFormation>();
+            using (var context = new AvisEntities())
+            {
+                List<Formation> formation = context.Formation.OrderBy(x => Guid.NewGuid()).Take(4).ToList();
+                foreach (Formation formationdetail in formation)
+                {
+                    DetailFormation detailsFormations = ObtenirDetailsFormation(formationdetail.NomSeo);
+                    if (detailsFormations != null)
+                        detailFormationListe.Add(detailsFormations);
+                }
+            }
+            return detailFormationListe;
+
+        }
+
+        public DetailFormation ObtenirDetailsFormation(string nomSeo)
+        {
+            DetailFormation detailFormationEntity = null;
+            using (var context = new AvisEntities())
+            {
+
+                Formation detailEntity = context.Formation.SingleOrDefault(f => f.NomSeo == nomSeo);
+
+                if (detailEntity != null)
+                {
+                    detailFormationEntity = new DetailFormation(detailEntity) { MoyenneDesVotes = 0, NombreVotant = 0 };
+
+                    if (detailEntity.Avis.Any())
+                    {
+                        detailFormationEntity.MoyenneDesVotes = Math.Round(detailEntity.Avis.Average(a => a.Note),1);
+                        detailFormationEntity.NombreVotant = detailEntity.Avis.Count();
+                        detailFormationEntity.Avis = detailEntity.Avis.ToList();
+                    }
+
+                }
+
+            }
+            return detailFormationEntity;
+        }
+
+        public static bool ObtenirFormation(string nomSeo, out Formation formation)
+        {
+            formation = null;
+            using (var context = new AvisEntities())
+            {
+                var formationContext = context.Formation.SingleOrDefault(f => string.Compare(f.NomSeo, nomSeo, true) == 0);
+                if (formationContext != null)
+                {
+                    formation = formationContext;
+                    return true;
+                }
+                return false;
+
+            }
+
+        }
+    }
+}
